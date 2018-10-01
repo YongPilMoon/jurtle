36d3865a5fbf9555096785f7907c64dbde0ecfdc
yongpilMoon <m71661@gmail.com>
2018-10-02 01:09:14 +0900

    react-ga 패키지를 설치하고 ga를 연동한다

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import Root from './Root';
 import registerServiceWorker from './registerServiceWorker';
 import 'styles/base.scss';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-126745807-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 
 ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();

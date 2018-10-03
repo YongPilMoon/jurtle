14e4440c30addcf1e74d0233e2ca5b4f68e1ab01
yongpilMoon <m71661@gmail.com>
2018-10-03 23:39:36 +0900

    PostList 페이지의 title 태그를 지정한다

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -3,12 +3,16 @@ import PageTemplate from 'components/common/PageTemplate';
 import ListWrapper from 'components/list/ListWrapper';
 import ListContainer from 'containers/list/ListContainer';
 import CategoryContainer from 'containers/list/CategoryConatiner';
+import {Helmet} from "react-helmet";
 
 const ListPage = ({match}) => {
   const { page = 1, tag } = match.params;
 
   return (
     <PageTemplate>
+      <Helmet>
+        <title>Development blog jurtle</title>
+      </Helmet>
       <CategoryContainer/>
       <ListWrapper>
         <ListContainer
